d676df07404ce1352c13274a2b9a37d18e4513a6
Lijian <727402046@qq.com>
2022-12-10 21:34:58 +0800

    feat:优化代码

--- bohan/examples/server/tcpserver.cpp
+++ bohan/examples/server/tcpserver.cpp
@@ -3,7 +3,7 @@
  * @Date: 2022-11-06 10:53:07
  * @FilePath: /Bohan/bohan/examples/server/tcpserver.cpp
  * @LastEditors: bohan.lj
- * @LastEditTime: 2022-11-20 13:34:08
+ * @LastEditTime: 2022-12-10 21:32:04
  * @Description: srouce_code
  */
 #include "net/Connection.h"
@@ -29,15 +29,12 @@ int main()
     if (ret == NetOptErrorCode::NET_OPT_ERROR)
 		return (int)ret;
 
-	ret = net_listen("127.0.0.1", 10086, serv_callback, NULL);
-	if (ret == NetOptErrorCode::NET_OPT_ERROR)
+    ret = net_listen("127.0.0.1", 10086, serv_callback, NULL);
+    if (ret == NetOptErrorCode::NET_OPT_ERROR)
     {
         return ret;
     }
-
-	printf("now enter the event loop...\n");
-
-	net_eventloop();
-
+    printf("now enter the event loop...\n");
+    net_eventloop();
     return 0;
 }
