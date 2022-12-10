74dec9913d9138802eff4795aa782f256bab4ee4
Lijian <727402046@qq.com>
2022-12-10 22:59:17 +0800

    feat:add add time class

--- bohan/base/Util.cc
+++ bohan/base/Util.cc
@@ -3,7 +3,7 @@
  * @Date: 2022-11-05 22:08:50
  * @FilePath: /Bohan/bohan/base/Util.cc
  * @LastEditors: bohan.lj
- * @LastEditTime: 2022-11-05 23:02:01
+ * @LastEditTime: 2022-12-10 22:57:19
  * @Description: srouce_code
  */
 #include "Util.h"
@@ -29,5 +29,64 @@ uint64_t get_current_tick()
 	current_tick = tval.tv_sec * 1000L + tval.tv_usec / 1000L;
 	return current_tick;
 #endif
+}
+
+unsigned long long gettime_forday_ms()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * (unsigned long long)1000 + tv.tv_usec/1000;	
+}
+unsigned long long gettime_forday_us()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * (unsigned long long)1000000 + tv.tv_usec;
+}
+
+DateTimeEntity gettime_now()
+{
+    DateTimeEntity  date;
+#ifdef OS_WIN
+    SYSTEMTIME tm;
+    GetLocalTime(&tm);
+    date.year     = tm.wYear;
+    date.month    = tm.wMonth;
+    date.day      = tm.wDay;
+    date.hour     = tm.wHour;
+    date.min      = tm.wMinute;
+    date.sec      = tm.wSecond;
+    date.ms       = tm.wMilliseconds;
+#else
+    struct timeval tv;
+    struct tm* tm = NULL;
+    gettimeofday(&tv, NULL);
+    time_t tt = tv.tv_sec;
+    tm = localtime(&tt);
+    date.year     = tm->tm_year + 1900;
+    date.month    = tm->tm_mon  + 1;
+    date.day      = tm->tm_mday;
+    date.hour     = tm->tm_hour;
+    date.min      = tm->tm_min;
+    date.sec      = tm->tm_sec;
+    date.ms       = tv.tv_usec/1000;
+#endif
+    return date;
+}
+
+char* datetime_fmt(DateTimeEntity* dt, char* buf)
+{
+    sprintf(buf, "%04d-%02d-%02d %02d:%02d:%02d",
+        dt->year, dt->month, dt->day,
+        dt->hour, dt->min, dt->sec);
+    return buf;
+}
+char* datetime_fmt_ms(DateTimeEntity* dt, char* buf)
+{
+    sprintf(buf, "%04d-%02d-%02dT%02d:%02d:%02d.%03dZ",
+        dt->year, dt->month, dt->day,
+        dt->hour, dt->min, dt->sec,
+        dt->ms);
+    return buf;
 }
 }
--- bohan/base/Util.h
+++ bohan/base/Util.h
@@ -3,7 +3,7 @@
  * @Date: 2022-11-05 22:08:34
  * @FilePath: /Bohan/bohan/base/Util.h
  * @LastEditors: bohan.lj
- * @LastEditTime: 2022-11-05 23:04:23
+ * @LastEditTime: 2022-12-10 22:56:43
  * @Description: srouce_code
  */
 
@@ -23,10 +23,48 @@
 #include "os_type.h"
 
 namespace bohan{
+
+typedef struct DateTimeEntity{
+    int year;
+    int month;
+    int day;
+    int hour;
+    int min;
+    int sec;
+    int ms;
+};
+
+#ifdef _MSC_VER
+struct timezone {
+    int tz_minuteswest; /* of Greenwich */
+    int tz_dsttime;     /* type of dst correction to apply */
+};
+
+#include <sys/timeb.h>
+int gettimeofday(struct timeval *tv, struct timezone *tz) {
+    struct _timeb tb;
+    _ftime(&tb);
+    if (tv) {
+        tv->tv_sec =  (long)tb.time;
+        tv->tv_usec = tb.millitm * 1000;
+    }
+    if (tz) {
+        tz->tz_minuteswest = tb.timezone;
+        tz->tz_dsttime = tb.dstflag;
+    }
+    return 0;
+}
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 uint64_t get_current_tick();
+unsigned long long gettime_forday_ms();
+unsigned long long gettime_forday_us();
+DateTimeEntity gettime_now();
+char* datetime_fmt(DateTimeEntity* dt, char* buf);
+char* datetime_fmt_ms(DateTimeEntity* dt, char* buf);
 #ifdef __cplusplus
 }
 #endif
